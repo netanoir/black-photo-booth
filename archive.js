5c9eb76471fdf1dda3baa2e84f14634b8e19956d
neta <netanyabomani@gmail.com>
2020-05-23 01:17:21 -0400

    fix are.na api; update css

--- archive.js
+++ archive.js
@@ -1,23 +1,33 @@
 window.addEventListener('DOMContentLoaded', async () => {
 
     // are.na api
-    const URL = "https://api.are.na/v2/channels/black-photo-booth-ljgcx4v33om";
+    const URL = "https://api.are.na/v2/channels/black-photo-booth-ljgcx4v33om?page=1&per=100";
     const apiOptions = { method: "GET" };
     const response = await fetch(URL, apiOptions);
     const result = await response.json();
     // console.log(result);
 
+    // randomize
+    var numOfPhotos = result.length;
+    var randomPhoto = Math.floor( Math.random() * numOfPhotos );
+
+
     // loop thru bpb are.na channel json image urls
-    for (let i = 0; i < result.length; i += 1) {
-        // store bpb are.na channel json image urls in variable
-        var imgURL = result.contents[i].image.large.url;
-        console.log(imgURL);
+    for (let i = 0; i < numOfPhotos; i += 1) {
 
-        let photo = new Image();
-        photo.src = imgURL;
-        photo.setAttribute('class', 'photo');
+        // if (result.contents[i] !== undefined) {
 
-        // add photo to archive booth
-        document.getElementById("booth").appendChild(photo);
+            // store bpb are.na channel json image urls in variable
+            var imgURL = result.contents[i].image.display.url;
+            console.log(imgURL);
+
+            let photo = new Image();
+            photo.src = imgURL;
+            photo.setAttribute('class', 'photo');
+
+            // add photo to archive booth
+            document.getElementById("booth").appendChild(photo);
+        //   }
+
     }
 });
